193b3de554c9c7b6a428ca00d4f253e328e0d5ba
kilo <h_x_123@163.com>
2019-04-30 16:42:53 +0800

    新增swagger配置

--- src/main/java/com/mit/pyramid/PyramidApplication.java
+++ src/main/java/com/mit/pyramid/PyramidApplication.java
@@ -2,8 +2,10 @@ package com.mit.pyramid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PyramidApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/mit_pyramid/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.mit_pyramid.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+@Configuration
+public class SwaggerConfig {
+    public ApiInfo createApi(){
+        return new ApiInfoBuilder().title("创客系统").description("打造真正的裂变用户平台").contact(new Contact("Core","10.8.159.28","123@123")).build();
+    }
+
+    /**
+     * basepackage 生成api所在包
+     * @return
+     */
+    @Bean
+    public Docket createDoc(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(createApi()).select().apis(RequestHandlerSelectors.basePackage("com.cc.mit_pyramid.controller" )).build();
+    }
+}
