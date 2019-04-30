444457b329a7d748ed074401d2bed79c8a5ab59b
Ery <469538970@qq.com>
2019-04-30 16:16:54 +0800

    Init

---
+++ src/main/java/com/mit/pyramid/PyramidApplication.java
@@ -0,0 +1,13 @@
+package com.mit.pyramid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyramidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PyramidApplication.class, args);
+    }
+
+}
