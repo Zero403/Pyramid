d8d0ec1ab7dfca485dbe5656e43a226f03fb4f28
zhFinal <zhqq6736@163.com>
2019-05-02 16:51:08 +0800

    1.删除Cookie存储数据的方式，若需要解析token，后台请使用相关方法。 2.优化了验证邀请码的方法。

--- src/main/java/com/mit/pyramid/common/constsys/SystemConst.java
+++ src/main/java/com/mit/pyramid/common/constsys/SystemConst.java
@@ -1,22 +1,36 @@
 package com.mit.pyramid.common.constsys;
 
 /**
- *@Author feri
- *@Date Created in 2019/4/28 17:30
+ * @Author feri
+ * @Date Created in 2019/4/28 17:30
  */
 public class SystemConst {
     //返回码
-    public static final int OK=1000;
-    public static final int ERROR=1001;
+    public static final int OK = 1000;
+    public static final int ERROR = 1001;
 
     //短信模板
-    public static final String DTMB="SMS_164470243";
-    public static final String YZMMB="SMS_114390520";
+    public static final String DTMB = "SMS_164470243";
+    public static final String YZMMB = "SMS_114390520";
 
     //秘钥
-    public static final String TOKENKEY="PTeW11QpXc+IKUAcO8e8FA==";
+    public static final String TOKENKEY = "PTeW11QpXc+IKUAcO8e8FA==";
 
+    /**
+     * 密码解密用key
+     */
     public static final String PASS = "4484AD3CBA81DB0978D699139CB973B8";
+
+    public static final int UPTO2 = 3;
+    public static final int UPTO3 = UPTO2 + 3;
+    public static final int UPTO4 = UPTO3 + 3;
+    public static final int UPTO6 = UPTO4 + 3;
+    public static final int UPTO7 = UPTO6 + 3;
+    public static final int UPTO8 = UPTO7 + 3;
+    public static final int UPTO10 = UPTO8 + 3;
+    public static final int UPTO11 = UPTO10 + 3 * 3;
+    public static final int UPTO12 = UPTO11 + 3 * 3 * 3;
+    public static final int UPTO13 = UPTO12 + 3 * 3 * 3;
 
 
 }
