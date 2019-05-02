9e50242d40f638f820edaa8ec75de72b5ebaec35
zhFinal <zhqq6736@163.com>
2019-05-02 13:06:57 +0800

    优化注册，关联邀请人数表和用户状态表

--- src/test/java/com/mit/pyramid/PyramidApplicationTests.java
+++ src/test/java/com/mit/pyramid/PyramidApplicationTests.java
@@ -10,6 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class PyramidApplicationTests {
 
+    /**
+     * 测试邀请码用，邀请码DD6264CB5540DF3126AD1F5E9FF5D0F1(uid == 2)
+     */
     @Test
     public void contextLoads() {
         String inviteCode = InviteCodeAPI.createInviteCode(2);
