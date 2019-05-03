c50fc206f997025e0f0477f59a22b94e0e36546a
kilo <h_x_123@163.com>
2019-05-03 15:56:53 +0800

    添加用户相关展示

--- src/main/java/com/mit/pyramid/common/advice/ExceptionAdvice.java
+++ src/main/java/com/mit/pyramid/common/advice/ExceptionAdvice.java
@@ -1,11 +1,17 @@
 package com.mit.pyramid.common.advice;
 
+import com.mit.pyramid.common.util.ResultUtil;
+import com.mit.pyramid.common.vo.ResultVO;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@ResponseBody
 public class ExceptionAdvice {
 
-    public void numberFormatException(NumberFormatException e) {
-
+    @ExceptionHandler
+    public ResultVO numberFormatException(Exception e) {
+        return ResultUtil.setERROR(e.getMessage());
     }
 }
