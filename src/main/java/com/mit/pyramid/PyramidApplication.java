f31a5ab119140b38d0ac73b30359ee8c7a1cf7ef
Ery <469538970@qq.com>
2019-05-03 12:04:22 +0800

    config整合到application

--- src/main/java/com/mit/pyramid/PyramidApplication.java
+++ src/main/java/com/mit/pyramid/PyramidApplication.java
@@ -4,20 +4,29 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @MapperScan("com.mit.pyramid.dao")
-public class PyramidApplication {
+public class PyramidApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PyramidApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PyramidApplication.class);
     }
 
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
     }
+
+
 }
