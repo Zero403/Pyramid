8f2be36502f69ea71fc0926763355760f99aa2e5
KylieChenchen <KylieChenchen@163.com>
2019-05-04 10:14:43 +0800

    rankTask

---
+++ src/main/java/com/mit/pyramid/task/RankTask.java
@@ -0,0 +1,30 @@
+package com.mit.pyramid.task;
+
+import com.mit.pyramid.service.FUserBasicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RankTask {
+
+    @Autowired
+    private FUserBasicService fUserBasicService;
+    // 每天早上8点刷新等级排名榜只显示前50
+    @Scheduled(cron = "0 0 8 ? * * *")
+    public void rankList(){
+
+    }
+
+    // 每天早上8点刷新邀请榜只显示前50
+    @Scheduled(cron = "0 0 8 ? * * *")
+    public void inviteList(){
+
+    }
+
+
+}
+
+
+
+
